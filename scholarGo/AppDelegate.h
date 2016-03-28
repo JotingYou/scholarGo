6a9e31aebf9094869c8d7f6d584b9ab4176b26b2
JotingYou <youjoting@gamil.com>
2016-03-28 20:34:57 +0800

    Initial Commit

---
+++ scholarGo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scholarGo
+//
+//  Created by 姚家庆 on 16/3/28.
+//  Copyright © 2016年 姚家庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scholarGo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scholarGo
+//
+//  Created by 姚家庆 on 16/3/28.
+//  Copyright © 2016年 姚家庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
